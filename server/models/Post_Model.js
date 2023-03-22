ac4abadf65319f9a77debbe521ba15201cdb800e
Shivam Gupta <gupta.shivam2604@gmail.com>
2023-03-22 23:24:56 +0530

    Ready to build

--- server/models/Post_Model.js
+++ server/models/Post_Model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
   {
-    title      : { type: String,required: true, minlength: 10,maxlength: 80, },
-    description: { type: String, required: true, minlength: 5, maxlength: 1024, required: true,},
+    title      : { type: String,required: true },
+    description: { type: String, required: true, required: true,},
     author     : { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, },
     views      : { type: Number, default: 1,min: 1 },
     upvotes    : { type: [mongoose.Schema.Types.ObjectId], ref: "User", default: []},
